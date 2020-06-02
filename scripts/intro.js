1eec7e6b2f153ea1e09f25c2012372c965d8258c
Emerson Souza <40323952+EmesonSSouza@users.noreply.github.com>
2020-06-02 18:20:48 -0300

    "fix(aula2): Criando html, js e css da  página de cadastro"

---
+++ scripts/intro.js
@@ -0,0 +1,56 @@
+//váriaveis
+//let myvar = "Hello"
+//const myconst = "He"
+//document.write(myconst + myvar)
+
+//Strings
+//"Isso é uma string"
+//'Isso é uma string'
+//`Isso é uma string`
+
+//Number
+//const n1 = 1
+//const n2 = 12
+//document.write(n1+n2)
+
+//Boolean - true ou false
+//const bTrue = true
+//const bFalse = false
+
+//Objeto possuem propriedade e funcionalidade - Estrutura de dados
+// const pessoa = {
+//   altura: "1,90m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     return "run forest"
+//   }
+// }
+// document.write(pessoa.correr())
+
+//Array - Vetores
+// const colecaoDeBolinha = ["Blue","Gren",5,{name:"My name"}]
+// document.write(colecaoDeBolinha[3].name)
+
+// Funções - funcionalidades 
+
+//registrar função
+// function sayMyName(name) {
+//   document.write(name)
+// }
+// //executar
+// sayMyName("Emerson")
+// sayMyName("Hellen")
+
+//Condicional 
+// const notaFinal = 7
+// if(notaFinal < 5) {
+//   document.write("Reprovado")
+// }else {
+//   document.write("Aprovado")
+// }
+
+//Loop repetições
+// for(i = 0; i <10; i++) {
+//   document.write(`<p>${i}<\p>`)
+// }
