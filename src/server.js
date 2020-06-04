4ba29583a99e625e19bf9c48a6dc33033b563d76
Emerson Souza <40323952+EmesonSSouza@users.noreply.github.com>
2020-06-04 14:22:22 -0300

    criar backend, servidor e configurar estrutura

--- src/server.js
+++ src/server.js
@@ -1,11 +1,28 @@
 const express = require("express")
 const server = express()
 
+//configurar pasta publica
+server.use(express.static("public"))
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 //configurar caminhos da aplicação
 //página inicial
-
 server.get("/",(req, res) => {
-    res.send("cheguei aqui")
+    return res.render("index.html", { title:"Um titulo"})
+})
+
+server.get("/create-point",(req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search",(req, res) => {
+    return res.render("search-results.html")
 })
 
 //ligar o servidor 
